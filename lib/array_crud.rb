caae8c63feaafcfa885442c7da8dffe5ca13e83e
Mark Hernandez <m.hern1415@gmail.com>
2019-04-29 19:59:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   numbers = ["one", "two", "three", "four"]
-  numbers.push("five")
+  numbers.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
